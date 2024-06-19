cf72318e4e9eed5d8bf67d2f818d91c1e1a400f5
sar-bc <sar-bc@mail.ru>
2024-06-19 22:51:46 +0400

    third

---
+++ third/todos/todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def signupuser(request):
+    return render(request, 'todo/signupuser.html')
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
